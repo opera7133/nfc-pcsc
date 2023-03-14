57ca803291dab48687c5560f451361f94f906fb6
opera7133 <ainznino@pm.me>
2023-03-14 14:53:41 +0900

    change to pcsclite

--- test/tests.js
+++ test/tests.js
@@ -3,9 +3,9 @@
 import test from 'ava';
 import mock from 'mock-require';
 
-// mock @pokusew/pcsclite to allow to simulate cards
+// mock pcsclite to allow to simulate cards
 import pcscliteMock from './helpers/pcsclite-mock';
-mock('@pokusew/pcsclite', pcscliteMock);
+mock('pcsclite', pcscliteMock);
 const NFC = require('../src/NFC').default;
 
 
